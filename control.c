f1bce61c0b179da0d97a0f2ccc81e03443eec4ce
iwang2 <iwang2@stuy.edu>
2017-12-10 18:14:29 -0500

    fixed the seg fault in -v and -r

--- control.c
+++ control.c
@@ -29,45 +29,37 @@ int main (int argc, char * argv[]){
       sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0600);
       if (sd == -1) {
 	printf("semaphore already exists\n");
-      } else {
-	// GO BACK TO REPLACE WITH UNION
-	semctl(sd, 0, SETVAL, 1); //setting sem value to 1
-	smd = shmget(KEY, sizeof(int), IPC_CREAT);
-	int * size = shmat(smd, 0, 0);
-	* size = 0;
-	fd = open("story.txt", O_TRUNC | O_CREAT, 0644);
       }
-      close(fd);
+      else {
+	fd = open("story.txt", O_TRUNC | O_CREAT, 0644);
+	semctl(sd, 0, SETVAL, 1);	
+	smd = shmget(KEY, sizeof(int), IPC_CREAT);
+	close(fd);
+      }
     }
 
     // VIEWING STORY
     if (!strcmp(argv[i], "-v")) {
-      fd = open("story.txt", O_RDONLY);
-      char * buff;
 
       struct stat st;
       stat("story.txt", &st);
       int size = (int)st.st_size;
+      printf("size of story.txt: %d\n", size);
 
-      read(fd, buff, size);
-      printf("%s\n", buff);
-
+      char * buff = malloc(size);
+      fd = open("story.txt", O_RDONLY | O_TRUNC);
+      int rr = read(fd, buff, size);      
+      if(rr == -1){
+	printf("error reading story.txt: %s\n", strerror(errno));
+      }else{
+	printf("%s\n", buff);
+      }
+      free(buff);
       close(fd);
     }
 
     // REMOVAL
-    if (!strcmp(argv[i], "-r")) {      
-      //read file into buff
-      fd = open("story.txt", O_RDONLY);
-      char * buff;
-      
-      struct stat st;
-      stat("story.txt", &st);
-      int size = (int)st.st_size;
-      
-      read(fd, buff, size);
-      close(fd);
-      remove("story.txt");
+    if (!strcmp(argv[i], "-r")) {
 
       //removing semaphore
       sd = semget(KEY, 1, 0);
@@ -86,8 +78,20 @@ int main (int argc, char * argv[]){
       } else {
 	printf("shared memory removed\n");
       }
+      
+      //read file into buff
+      struct stat st;
+      stat("story.txt", &st);
+      int size = (int)st.st_size;
+      
+      fd = open("story.txt", O_RDONLY);
+      char * buff = malloc(size);
+      read(fd, buff, size);
+      close(fd);
+      remove("story.txt");
 
       printf("\nFINAL STORY:\n%s\n", buff);
+      free(buff);
     }
   }
 }
